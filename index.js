ed2b1fd96aeaab5b663b06ab50445faa3ef6e72a
NguyenPhucTuong <tuongdio112233@gmail.com>
2025-11-02 11:56:12 +0700

    update new html and js

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello world")
+
+let fullName = "PhucTuong"
+let age = 25;
+let firstName = "Nguyen"
+let lastName = "Phuc Tuong"
+let email = "123@gamil.com"
+let create = "01/11/2025"
+let update = "02/11/2025"
+let deleteDate = "3/11/2025"
+console.log("firstName.lastName,email,create.update,deleteDate")
+let str1 = 'hello'
+let str2 = 'world'
+let str = "Heloo World"
+console.log('số ký tự trong chuỗi là:', str.length)
